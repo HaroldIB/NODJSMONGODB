18bcfa34f9232b890de082f48d5e46f53a73c4f2
Harold <haroldbustamante22@gmail.com>
2022-04-22 16:01:44 -0400

    Página Lista para trabajar con la base de datos MONGODB

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
-import express from 'express';
-console.log("Hola Mundo !");
+import app from './app';
+import {Server as WebsocketServer} from 'socket.io';
+import http from 'http';
+import { connectDB } from './db';
+import sockets from './sockets';
 
+connectDB();
+
+const server = http.createServer(app);
+const httpserver = server.listen(3000);
+console.log("Escuchado Servidor en Puerto 3000");
+
+const io = new WebsocketServer(httpserver)
+sockets(io);
