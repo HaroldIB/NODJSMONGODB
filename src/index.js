4f2eb7d3f5c0d472c54fe759c25f7e8ecfbbd33d
Harold <haroldbustamante22@gmail.com>
2022-04-23 18:46:49 -0400

    Empezando la subida a HEROKU

--- src/index.js
+++ src/index.js
@@ -1,14 +1,15 @@
 import app from './app';
-import {Server as WebsocketServer} from 'socket.io';
+import { Server as WebsocketServer } from 'socket.io';
 import http from 'http';
 import { connectDB } from './db';
 import sockets from './sockets';
+import { PORT } from "./config"
 
 connectDB();
 
 const server = http.createServer(app);
-const httpserver = server.listen(3000);
-console.log("Escuchado Servidor en Puerto 3000");
+const httpserver = server.listen(PORT);
+console.log("Escuchado Servidor en Puerto ", PORT);
 
 const io = new WebsocketServer(httpserver)
 sockets(io);
